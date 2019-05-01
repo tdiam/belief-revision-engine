bebed7bab152f6b97a6201d16fe78cf8955753c7
bdomanski <domanski.brian@gmail.com>
2019-05-01 12:50:13 +0200

    Added consistency check

--- consistency.py
+++ consistency.py
@@ -1,6 +1,21 @@
+from sympy import And
+from sympy.logic.inference import satisfiable
+
 """
 Checks if a given formula is consistent with the current belief base
+Given two cnfs a, b:
+    if a U b is not satisfiable, b does not follow from a
 """
 def is_consistent(bb, formula):
+    for belief in bb:
+        test = combine_cnfs(belief, formula)
+        if satisfiable(test) is False:
+            print("INCONSISTENT")
+            return False
+        
     return True
+
+def combine_cnfs(a, b):
+    return And(a, b)
+
     
